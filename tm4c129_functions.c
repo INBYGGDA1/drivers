880a58a96380c23dcc8bf5c0f77c916bbb8ff505
pontus <github@doughnut.mailer.me>
2023-10-07 21:25:47 +0200

    Function descriptions

--- tm4c129_functions.c
+++ tm4c129_functions.c
@@ -23,17 +23,14 @@
 #include "driverlib/adc.h"
 //=============================================================================
 #include "driverlib/uart.h"
+#include "drivers/buttons.h"
 #include "inc/hw_memmap.h"
 //=============================================================================
 #include "CF128x128x16_ST7735S.h"
 //=============================================================================
 #include "grlib/grlib.h"
 #include "utils/uartstdio.h"
-
-//=============================================================================
-#define MICROPHONE (1 << 0)
-#define JOYSTICK (1 << 1)
-#define ACCELEROMETER (1 << 2)
+#include "tm4c129_functions.h"
 
 //=============================================================================
 // The error routine that is called if the driver library
@@ -47,7 +44,20 @@ void __error__(char *pcFilename, uint32_t ui32Line) {
 #endif
 
 //=============================================================================
-// Configure the UART.
+// clang-format off
+/*
+ * Function: ConfigureUART
+ * ------------------------------
+ *  Autmatically configure the UART using the TivWare peripheral driver library 
+ *
+ *  Settings:
+ *    Baud-Rate: 115200
+ *    Clock freq: 16000000
+ *
+ *  returns:
+ *    N/A
+ */
+// clang-format on
 //=============================================================================
 void ConfigureUART() {
   SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOA);
@@ -60,7 +70,25 @@ void ConfigureUART() {
 }
 
 //=============================================================================
-// Helper function to reinitialize the ADC to sample on another channel
+// clang-format off
+/*
+ * Function: ADC_newSequence
+ * ---------------------------
+ * Reinitializes the ADC to sample from a different channel.
+ *
+ * parameters:
+ *   ui32base - The ADC base address, defined in the TivaWare peripheral library.
+ *   ui32SequenceNum - Sequence number for ADC sampling. Valid values are 0, 1, 2, 3.
+ *                     Refer to datasheet p.1180 for details.
+ *   ui32ADC_Channel - The analog input channel to sample from.
+ *                     Refer to datasheet p.1179 for available channels.
+ *   ui32Samples - Number of samples to read in the sequence.
+ *                 Value is dependent on SequenceNum. See datasheet p.1180 for specifics.
+ *
+ * returns:
+ *   None. The function modifies hardware registers to change ADC settings.
+ */
+// clang-format on
 //=============================================================================
 void ADC_newSequence(uint32_t ui32base, uint32_t ui32SequenceNum,
                      uint32_t ui32ADC_Channel, uint32_t ui32Samples) {
@@ -95,7 +123,29 @@ void ADC_newSequence(uint32_t ui32base, uint32_t ui32SequenceNum,
 }
 
 //=============================================================================
-// Helper function to sample the data
+// clang-format off
+/*
+ * Function: sampleData
+ * ---------------------------
+ *  Trigger the sampling sequence for the specified sequence
+ *
+ *  parameters:
+ *    ui32base - The ADC base address
+ *    ui32SequenceNum - Sequence number for ADC sampling. Valid values are 0, 1, 2, 3. 
+ *                      Refer to datasheet p.1180 for details.
+ *    ui32ADC_Channel - The analog input channel to sample from.
+ *                      Refer to datasheet p.1179 for available channels
+ *    ui32samples - Number of samples to initialize the sequence to.
+ *                  The values is dependent on SequenceNum.
+ *                  Refer to datasheet p.1180 for details.
+ *    *ui32SamplesRead - Assigns the samples read after the sampling sequence has been triggered.
+ *    *buffer - Pointer to the location where the ADC will store the values samples.
+ *    newSequence - Is a flag to indicate if a new sequence should be configured before sampling.
+ *
+ *    returns:
+ *      N/A
+ */
+// clang-format on
 //=============================================================================
 void sampleData(uint32_t ui32base, uint32_t ui32SequenceNum,
                 uint32_t ui32ADC_Channel, uint32_t ui32Samples,
@@ -125,7 +175,20 @@ void sampleData(uint32_t ui32base, uint32_t ui32SequenceNum,
 }
 
 //=============================================================================
-// Helper function to calculate the average of the collected ADC samples
+// clang-format off
+/*
+ * Function: calculate_average
+ * ---------------------------------------
+ *  Helper function to calculate the average of the collected ADC samples
+ *  parameters:
+ *    ui32samples - The number of samples in the buffer.
+ *    *buffer - pointer to the array storing the samples.
+ *    *averageReturned - Pointer to store the average value of the buffer
+ *
+ *  returns:
+ *    N/A
+ */
+// clang-format on
 //=============================================================================
 void calculate_average(uint32_t ui32samples, uint32_t *buffer,
                        uint32_t *averageReturned) {
@@ -142,48 +205,117 @@ void calculate_average(uint32_t ui32samples, uint32_t *buffer,
 }
 
 //=============================================================================
-// Helper function to enable any sensor
+// clang-format off
+/*
+ * Function: SENSOR_enable
+ * ---------------------------
+ *  Helper function to enable any sensor on the MKII 
+ *
+ *  parameters:
+ *    enablePeripheral - Valid inputs: MICROPHONE | JOYSTICK | ACCELEROMETER | BUZZER | BUTTON_UP 
+ *                        | BUTTON_DOWN
+ *   
+ *  returns:
+ *  N/A
+ */
+// clang-format on
 //=============================================================================
 void SENSOR_enable(uint32_t enablePeripheral) {
-  // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-  // ADC Pin Configuration
-  //
-  // Component    Axis     Pin    GPIO       Channel       AIN
-  // --------------------------------------------------------
-  // Microphone   N/A      PE5    GPIO_PIN_5  ADC_CTL_CH8  AIN8
-  //
-  // Joystick     X-Axis   PE4    GPIO_PIN_4  ADC_CTL_CH9  AIN9
-  //              Y-Axis   PE3    GPIO_PIN_3  ADC_CTL_CH0  AIN0
-  //
-  // Gyroscope    X-Axis   PE0    GPIO_PIN_0  ADC_CTL_CH3  AIN3
-  //              Y-Axis   PE1    GPIO_PIN_1  ADC_CTL_CH2  AIN2
-  //              Z-Axis   PE2    GPIO_PIN_2  ADC_CTL_CH1  AIN1
+  // Pin Config MKII
+  // Comp  Axis  Pin  GPIO  Port  Ch    AIN
+  // ---------------------------------------
+  // Mic   N/A   PE5  P5    PE    CH8   A8
+  // Joy   X     PE4  P4    PE    CH9   A9
+  //       Y     PE3  P3    PE    CH0   A0
+  // Gyro  X     PE0  P0    PE    CH3   A3
+  //       Y     PE1  P1    PE    CH2   A2
+  //       Z     PE2  P2    PE    CH1   A1
+  // Buz   N/A   PF1  P1    PF    N/A   N/A
+  // Btn1  N/A   PL1  P1    PL    N/A   N/A
+  // Btn2  N/A   PL2  P2    PL    N/A   N/A
   // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
-  uint32_t gpio_pins_to_enable = 0;
+  uint32_t gpio_porte_pins_to_enable = 0;
+  uint32_t gpio_portf_pins_to_enable = 0;
+  uint32_t gpio_portl_pins_to_enable = 0;
+  uint32_t gpio_ports_to_enable = 0;
   // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
   // If the microphone should be enabled set the correct pins
   if (enablePeripheral & MICROPHONE) {
-    gpio_pins_to_enable |= GPIO_PIN_5;
+    gpio_porte_pins_to_enable |= GPIO_PIN_5;
+    gpio_ports_to_enable |= GPIO_PORTE_BASE;
   }
   // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
   // Should the Joystick be enabled set the corresponding pins
   if (enablePeripheral & JOYSTICK) {
-    gpio_pins_to_enable |= GPIO_PIN_3 | GPIO_PIN_4;
+    gpio_porte_pins_to_enable |= GPIO_PIN_3 | GPIO_PIN_4;
+    gpio_ports_to_enable |= GPIO_PORTE_BASE;
   }
   // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
   // Should the accelerometer be used set the corresponding pins
   if (enablePeripheral & ACCELEROMETER) {
-    gpio_pins_to_enable |= GPIO_PIN_0 | GPIO_PIN_1 | GPIO_PIN_2;
+    gpio_porte_pins_to_enable |= GPIO_PIN_0 | GPIO_PIN_1 | GPIO_PIN_2;
+    gpio_ports_to_enable |= GPIO_PORTE_BASE;
   }
-
   // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-  // Enable all pins that should be set as ADC pins
-  GPIOPinTypeADC(GPIO_PORTE_BASE, gpio_pins_to_enable);
+  if (enablePeripheral & BUZZER) {
+    gpio_ports_to_enable |= GPIO_PORTF_BASE;
+    gpio_portf_pins_to_enable |= GPIO_PIN_1;
+  }
+  // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+  if (enablePeripheral & BUTTON_UP) {
+    gpio_ports_to_enable |= GPIO_PORTL_BASE;
+    gpio_portl_pins_to_enable |= GPIO_PIN_1;
+    // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    // Set the buttons to act as input
+  }
+  // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+  if (enablePeripheral & BUTTON_DOWN) {
+    gpio_ports_to_enable |= GPIO_PORTL_BASE;
+    gpio_portl_pins_to_enable |= GPIO_PIN_2;
+    // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    // Set the buttons to act as input
+  }
+  // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+  // Check which ports should be enabled
+  if (gpio_ports_to_enable & GPIO_PORTE_BASE) {
+
+    GPIOPort_init(SYSCTL_PERIPH_GPIOE);
+    // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    // Enable all pins that should be set as ADC pins
+    GPIOPinTypeADC(GPIO_PORTE_BASE, gpio_porte_pins_to_enable);
+  }
+  // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+  if (gpio_ports_to_enable & GPIO_PORTF_BASE) {
+    GPIOPort_init(SYSCTL_PERIPH_GPIOF);
+    // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    // Specify the buzzer to act as output
+    GPIOPinTypeGPIOOutput(GPIO_PORTF_BASE, GPIO_PIN_1);
+  }
+  // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+  if (gpio_ports_to_enable & GPIO_PORTL_BASE) {
+
+    GPIOPort_init(SYSCTL_PERIPH_GPIOL);
+    // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    // Set the buttons to act as input
+    GPIOPinTypeGPIOInput(GPIO_PORTL_BASE, gpio_portl_pins_to_enable);
+  }
 }
 
 //=============================================================================
-// Helper function to initialize any ADC module
+// clang-format off
+/*
+ * Function: ADC_init
+ * -------------------------
+ *  Helper function to initialize any ADC module
+ *
+ *  parameters:
+ *    ui32base - The ADC base address
+ *
+ *  returns:
+ *    N/A
+ */
+// clang-format on
 //=============================================================================
 void ADC_init(uint32_t ui32Base) {
   // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
@@ -194,7 +326,19 @@ void ADC_init(uint32_t ui32Base) {
 }
 
 //=============================================================================
-// Helper function to initalize any GPIO_PORTx_BASE
+// clang-format off
+/*
+ * Function: GPIOPort_init
+ * ---------------------------------
+ *  Helper function to initialize any GPIO_PORTx_BASE
+ *
+ *  parameters:
+ *    ui32GPIOx - The GPIO_PORTx_BASE to be enabled
+ *
+ *  returns:
+ *    N/A
+ */
+// clang-format on
 //=============================================================================
 void GPIOPort_init(uint32_t ui32GPIOx) {
   // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
@@ -205,20 +349,46 @@ void GPIOPort_init(uint32_t ui32GPIOx) {
 }
 
 //=============================================================================
-// Helper function to calculate absolute difference for uint32_t
+// clang-format off
+/*
+ * Function: abs_diff
+ * --------------------------------
+ *  Helper function to calculate the absolute differecen for an unsigned integer 
+ *  uint32_t.
+ *  parameters:
+ *    a - unsigned integer
+ *    b - unsigned integer
+ *
+ *  returns:
+ *    The absolute difference between a & b.
+ */
+// clang-format on
 //=============================================================================
 uint32_t abs_diff(uint32_t a, uint32_t b) {
   // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
   // Since the variables are defined as unsigned integers, I cant subtract a - b
-  // or vice versa since that could lead to a overflow issue. In this function i
-  // check if a > b, then return a - b, else if b > a I return b - a. In order
+  // or vice versa since that could lead to a underflow issue. In this function
+  // i check if a > b, then return a - b, else if b > a I return b - a. In order
   // to avoid taking the difference of a smaller unsigned integer with a greater
   // one.
   return (a > b) ? (a - b) : (b - a);
 }
 
 //=============================================================================
-// Helper function to clear the text
+// clang-format off
+/*
+ * Function: clear_text
+ * -------------------------------
+ *  Helper function to clear the text on the LCD
+ *
+ *  parameters:
+ *    *sContext - A pointer to context currently being used.
+ *    x - The x coordinate on the LCD screen.
+ *    y - The y coordinate on the LCD screen.
+ *    width - The width of the rectagle to be filled.
+ *    height - The heigh of the rectangle to be filled.
+ */
+// clang-format on
 //=============================================================================
 void clear_text(tContext *sContext, int x, int y, int width, int height) {
   // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
--- tm4c129_functions.h
+++ tm4c129_functions.h
@@ -1,7 +1,25 @@
 #ifndef HELPER_H
 #define HELPER_H
 #include <stdint.h>
+#include <sys/types.h>
 #include "grlib/grlib.h"
+#define MICROPHONE (1 << 0)
+#define JOYSTICK (1 << 1)
+#define ACCELEROMETER (1 << 2)
+#define BUZZER (1 << 3)
+#define BUTTON_UP (1 << 4)
+#define BUTTON_DOWN (1 << 5)
+
+#define GPIO_PORTE_SENSORS (1 << 0)
+#define GPIO_PORTF_SENSORS (1 << 1)
+#define GPIO_PORTL_SENSORS (1 << 2)
+
+// Linked list used in snake
+struct node {
+  uint32_t data;
+  struct node *next;
+};
+
 extern void ConfigureUART();
 extern void ADC_newSequence(uint32_t ui32base, uint32_t ui32SequenceNum,
                             uint32_t ui32ADC_Channel, uint32_t ui32Samples);
