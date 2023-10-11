8c81e196137653414280d7c7e1d81afb59efbdb0
Pontus Svensson <pontus.github@doughnut.anonaddy.com>
2023-10-11 13:55:01 +0200

    update

--- tm4c129_functions.h
+++ tm4c129_functions.h
@@ -10,16 +10,6 @@
 #define BUTTON_UP (1 << 4)
 #define BUTTON_DOWN (1 << 5)
 
-#define GPIO_PORTE_SENSORS (1 << 0)
-#define GPIO_PORTF_SENSORS (1 << 1)
-#define GPIO_PORTL_SENSORS (1 << 2)
-
-// Linked list used in snake
-struct node {
-  uint32_t data;
-  struct node *next;
-};
-
 extern void ConfigureUART();
 extern void ConfigureSystemClock(uint32_t frequency, uint32_t *systemClock);
 extern void UARTClearScreen();
@@ -34,5 +24,4 @@ extern void calculate_average(uint32_t ui32samples, uint32_t *buffer,
 extern void SENSOR_enable(uint32_t enablePeripheral);
 extern void PERIPH_init(uint32_t ui32Base);
 extern uint32_t abs_diff(uint32_t a, uint32_t b);
-extern void clear_text(tContext *sContext, int x, int y, int width, int height);
 #endif // !__HELPER__
